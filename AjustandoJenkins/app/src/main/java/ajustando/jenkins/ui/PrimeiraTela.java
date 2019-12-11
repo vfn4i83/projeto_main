d4345316a413c94cf30cb7207743fa02f968d33e
rafael <rafaelreg@outlook.com>
2019-12-11 19:25:05 -0300

    primeiro commit

---
+++ AjustandoJenkins/app/src/main/java/ajustando/jenkins/ui/PrimeiraTela.java
@@ -0,0 +1,16 @@
+package ajustando.jenkins.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ajustando.jenkins.R;
+
+public class PrimeiraTela extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_primeira_tela);
+    }
+}
